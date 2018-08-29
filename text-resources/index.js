83d2568219665945022f7495c921da0a49e2ca35
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-08-29 11:31:04 +0530

    Finished Sign In

--- text-resources/index.js
+++ text-resources/index.js
@@ -8,6 +8,10 @@ var tooltipInstances;
 var modalElements;
 var modalInstances;
 
+var otpFlag = false;
+
+var user;
+
 // Handle page onload events
 window.onload = function(){
 
@@ -47,7 +51,7 @@ window.onload = function(){
 	firebase.auth().useDeviceLanguage();
 
 	// Set up invisible reCAPTCHA
-	window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('signIn', {
+	window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('rp', {
 		'size': 'invisible',
 		'callback': function(response) {
 			// reCAPTCHA solved, allow signInWithPhoneNumber.
@@ -57,16 +61,75 @@ window.onload = function(){
 	// Send SMS for verification
 
 	document.getElementById("signIn").onclick= function(){
-		firebase.auth().signInWithPhoneNumber(document.getElementById("phoneNumber").value, window.recaptchaVerifier).then(function(confirmationResult){
-			// SMS sent. Prompt user to type the code from the message, then sign the user in with confirmationResult.confirm(code).
-			window.confirmationResult = confirmationResult;
-		}).catch(function(error){
-			// Error; SMS not sent
-		});
+
+		if(otpFlag){
+			otpFlag = false;
+			blockSignInModal();
+			confirmationResult.confirm(document.getElementById("phoneNumber").value).then(function (result) {
+				// User signed in successfully.
+					user = result.user;
+					unblockSignInModal();
+					signedIn();
+				}).catch(function (error) {
+					// User couldn't sign in (bad verification code?)
+					console.log("%cInvalid OTP. -->" + error, "background-color:#5555FF; color:#FF0000;");
+				});
+		}
+
+		else{
+			blockSignInModal();
+			firebase.auth().signInWithPhoneNumber(document.getElementById("phoneNumber").value, window.recaptchaVerifier).then(function(confirmationResult){
+				// SMS sent. Prompt user to type the code from the message, then sign the user in with confirmationResult.confirm(code).
+				window.confirmationResult = confirmationResult;
+				unblockSignInModal();
+				document.getElementById("signIn").innerHTML = "Verify OTP";
+				document.getElementById("phoneNumber").value = "";
+				document.getElementById("phno").innerHTML = "OTP";
+				document.getElementById("phoneNumber").focus();
+				otpFlag = true;
+			}).catch(function(error){
+				// Error; SMS not sent
+				console.log("%cError Sending SMS. -->" + error, "background-color:#5555FF; color:#FF0000;");
+			});
+		}
 	}
 
-	//TODO: Handle recieved code.
-	// Reference: https://firebase.google.com/docs/auth/web/phone-auth
+	// Callback after successful sign in
+
+	function signedIn(){
+		modalInstances[0].close();
+		document.getElementById("username").innerHTML = "<b>Sign Out</b>";
+		document.getElementById("username").classList.remove("modal-trigger");
+		document.getElementById("username").href = "";
+		document.getElementById("landingContents").style.display = "none";
+		document.getElementById("pageContents").style.display = "block";
+	}
+
+	// Callback after successful sign out
+
+	function signedOut(){
+		document.getElementById("username").innerHTML = "<b>Sign In</b>";
+		document.getElementById("username").classList.add("modal-trigger");
+		document.getElementById("username").href = "#signInModal";
+		document.getElementById("landingContents").style.display = "block";
+		document.getElementById("pageContents").style.display = "none";
+	}
+
+	// Block sign in modal
+
+	function blockSignInModal(){
+		document.getElementById("phoneOtpDiv").style.display = "none";
+		document.getElementById("signIn").classList.add("disabled");
+		document.getElementById("spinner").style.display = "block";
+	}
+
+	// Unblock sign in modal
+
+	function unblockSignInModal(){
+		document.getElementById("phoneOtpDiv").style.display = "block";
+		document.getElementById("signIn").classList.remove("disabled");
+		document.getElementById("spinner").style.display = "none";
+	}
 
 	/* Other Events */
 
